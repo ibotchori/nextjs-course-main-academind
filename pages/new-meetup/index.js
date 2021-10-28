1dd28ae46f4f5e75ace2381cbde5b2ec75977931
Irakli <koregm@gmail.com>
2021-10-28 19:24:33 +0400

    Adding A Form For Adding Meetups

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,1 +1,13 @@
 // our-domain/new-meetup
+import NewMeetupForm from "../../components/meetups/NewMeetupForm";
+
+const NewMeetupPage = () => {
+    
+  // this function receives data from NewMeetupForm component
+  function addMeetupHandler(enteredMeetupData) {
+    console.log(enteredMeetupData);
+  }
+  return <NewMeetupForm onAddMeetup={addMeetupHandler} /> // send addMeetupHandler function to NewMeetupForm with props
+};
+
+export default NewMeetupPage;
