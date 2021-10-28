70bbc3dff054e0d27c366460d0196d83a8cbe795
Irakli <koregm@gmail.com>
2021-10-28 18:19:17 +0400

    Rendering A List Of (Dummy) Meetups

--- pages/index.js
+++ pages/index.js
@@ -1,2 +1,31 @@
 // our-domain/
+import MeetupList from "../components/meetups/MeetupList";
 
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "A First Meetup",
+    image:
+      "https://images.unsplash.com/photo-1635361230615-26d866ea9370?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80",
+    address: "Some address 5, 12345 Some City",
+    description: "This is a first meetup",
+  },
+  {
+    id: "m2",
+    title: "A Second Meetup",
+    image:
+      "https://images.unsplash.com/photo-1635397186950-96a4d88d6181?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80",
+    address: "Some address 32, 45 Some City",
+    description: "This is a second meetup",
+  },
+];
+
+const HomePage = () => {
+  return (
+    <div>
+      <MeetupList meetups={DUMMY_MEETUPS}></MeetupList>
+    </div>
+  );
+};
+
+export default HomePage;
