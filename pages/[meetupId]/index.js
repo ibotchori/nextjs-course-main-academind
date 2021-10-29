18eac657aaeb3f6b13a13ab18e66f2fdf3feb415
Irakli <koregm@gmail.com>
2021-10-29 12:21:57 +0400

    Adding Custom Components & Styling With CSS Modules

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+const MeetupDetails = () => {
+  return (
+    <MeetupDetail
+      description="This is a first meetup"
+      address="Some Street 5, Some City"
+      title="First Meetup"
+      image="https://images.unsplash.com/photo-1635361230615-26d866ea9370?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80"
+    />
+  );
+};
+
+export default MeetupDetails;
