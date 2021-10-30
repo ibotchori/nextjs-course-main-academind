9f32fa840fd0624d98a2d24b87f8d687be690cbd
Irakli <koregm@gmail.com>
2021-10-30 21:06:06 +0400

    Working With Dynamic Path Params In getStaticProps

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -11,4 +11,24 @@ const MeetupDetails = () => {
   );
 };
 
+export async function getStaticProps(context) {
+  // Fetch date from API
+
+  const meetupId = context.params.meetupId;
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://images.unsplash.com/photo-1635361230615-26d866ea9370?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80",
+        id: meetupId,
+        title: "First Meetup",
+        address: "Some Street 5, Some City",
+        description: "This is a first meetup",
+      },
+    },
+  };
+}
+
 export default MeetupDetails;
