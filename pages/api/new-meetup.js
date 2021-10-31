1747a3768717aad7d99d54730c505dd4b3a4e9b8
Irakli <koregm@gmail.com>
2021-10-31 19:30:03 +0400

    Connecting & Querying a MongoDB Database

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,12 +1,37 @@
+import { MongoClient } from "mongodb";
+
 // /api/anew-meetup
 // POST /api/anew-meetup
 
-function handler(req, res) {
+async function handler(req, res) {
   if (req.method === "POST") {
+    /* Get request */
+    // export information from incoming data
     const data = req.body;
 
-    // export information from incoming data
-    const { title, image, address, description } = data
+    /* Store data in the database */
+    // Connect to mongodb cluster
+    const client = await MongoClient.connect(
+      "mongodb+srv://user:nP0WHombYLsf7ocx@cluster0.ynjjr.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+
+    // Call db method on the client object
+    const db = client.db();
+
+    // Access to collection
+    const meetupCollection = db.collection("meetups");
+
+    // insert one new document into this collection, with exported data object
+    const result = await meetupCollection.insertOne(data);
+
+    console.log(result);
+
+    // Close database connection
+    client.close();
+
+    /* Send back a response */
+    // send HTTP status code with status method, if something was inserted successfully and send a message.
+    res.status(201).json({ message: "Meetup Inserted!" });
   }
 }
 
